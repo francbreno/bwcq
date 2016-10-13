e1920c3c4105fb9effe0867429c56c40c3ee56f4
Breno <francbreno@gmail.com>
2016-10-13 08:53:41 -0300

    Rebase do código

--- src/app/conta-service.ts
+++ src/app/conta-service.ts
@@ -17,14 +17,6 @@ export class ContaService {
   }
 
   getContaMaiorSaldo(): Promise<Conta> {
-<<<<<<< HEAD
-    var contasOrdenadas = this.getContas().then(contas => { 
-      contas.sort((contaA, contaB): number => contaA.saldo - contaB.saldo)
-      return contas;
-    });
-  
-    return Promise.resolve(contasOrdenadas[0]);
-=======
     return Promise.resolve(this.getContas()
       .then(contas => {
         contas.sort((c1, c2) => c1.saldo - c2.saldo).reverse();
@@ -41,7 +33,5 @@ export class ContaService {
   }
 
   getContaMaiorRealizado() {
-
->>>>>>> 3f3c3ea985e1148eeee8ee4a612dd646368ce637
   }
 }
